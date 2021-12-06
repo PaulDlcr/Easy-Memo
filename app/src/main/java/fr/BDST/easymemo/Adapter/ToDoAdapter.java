b5458f3beec0eb0dde13d56937529515901129ad
PaulDlcr <84783007+PaulDlcr@users.noreply.github.com>
2021-12-06 19:55:36 +0100

    Add files via upload

---
+++ app/src/main/java/fr/BDST/easymemo/Adapter/ToDoAdapter.java
@@ -0,0 +1,51 @@
+package fr.BDST.easymemo.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import fr.BDST.easymemo.MainActivity;
+import fr.BDST.easymemo.Model.ToDoModel;
+import fr.BDST.easymemo.R;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder>{
+    private List<ToDoModel> todolist;
+    private MainActivity activity;
+
+    public ToDoAdapter(MainActivity activity){
+        this.activity = activity;
+    }
+
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    public void onBindViewHolder(ViewHolder holder, int position){
+        ToDoModel item = todolist.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+    }
+
+    public int getItemCount(){
+        return todolist.size();
+    }
+
+    private boolean toBoolean(int n) {
+        return n!=0;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        CheckBox task;
+
+        ViewHolder(View view){
+            super(view);
+            task = view.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
